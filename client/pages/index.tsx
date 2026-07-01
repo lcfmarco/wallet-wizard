dd54113b8de4c50254d783aa7a8ecc409f011a49
Marco Lam <marcolam1111@gmail.com>
2026-07-01 12:30:02 -0400

    feat: rough table draft of transaction table

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,17 +1,45 @@
 import React, { useEffect, useState } from 'react';
 
 function index() {
-  const [message, setMessage] = useState("Loading...");
+  const [transaction, setTransaction] = useState<any[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3100/api/home")
+    fetch("http://localhost:3100/api/transaction")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setMessage(data.message);
+        setTransaction(data);
       });
   }, []);
-  return <div>{message}</div>;
+  return (
+    <div>
+      <h1>Wallet Wizard</h1>
+
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Transaction Name</th>
+            <th>Category Name</th>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(transaction) && transaction.map((item, index) => (
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.category_name}</td>
+              <td>{new Date(item.date).toLocaleDateString()}</td>
+              <td>{(item.amount / 100).toLocaleString('en-us', { style: 'currency', currency: 'USD' })}</td>
+              <td>{new Date(item.created_at).toLocaleDateString('en-US', { timeZoneName: 'short'})}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default index;
