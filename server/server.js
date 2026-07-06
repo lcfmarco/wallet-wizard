73a6344b2c1a954649f296f3417c505b219f3cd9
Marco Lam <marcolam1111@gmail.com>
2026-07-06 15:10:52 -0400

    Add category API endpoint

--- server/server.js
+++ server/server.js
@@ -34,7 +34,23 @@ app.get("/api/transaction", async (req, res) => {
   try {
     client = await pool.connect();
     console.log('Got a connection from the pool');
-    const resp = await client.query('SELECT t.*, c.name as category_name FROM transaction t JOIN category c ON t.category_id = c.id');
+    const resp = await client.query('SELECT t.*, c.name as category_name FROM transaction t JOIN category c ON t.category_id = c.id WHERE t.deleted_at is NULL');
+    res.json(resp.rows);
+  } catch (err) {
+    console.error(err);
+    res.json({error: err});
+  } finally {
+    client?.release();
+  }
+});
+
+
+app.get("/api/category", async (req, res) => {
+  let client
+  try {
+    client = await pool.connect();
+    console.log('Got a connection from the pool');
+    const resp = await client.query('SELECT c.id, c.name as category_name, c.created_at FROM category as c');
     res.json(resp.rows);
   } catch (err) {
     console.error(err);
