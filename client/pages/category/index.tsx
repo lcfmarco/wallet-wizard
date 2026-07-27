ea1c58eb1536003c679e7412d3a836b32d96004e
Marco Lam <marcolam1111@gmail.com>
2026-07-27 16:40:07 -0400

    Add nav bar to the categories page

--- client/pages/category/index.tsx
+++ client/pages/category/index.tsx
@@ -1,8 +1,10 @@
 import CategoryList from '../../components/CategoryList';
+import Header from '../../components/Header';
 
 function CategoryPage() {
   return (
     <div className="page-container">
+      <Header />
       <div className="page-section">
         <CategoryList />
       </div>
